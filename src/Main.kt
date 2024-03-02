01476ab25fd84e81a2d03d611b17d2cd5fa8070a
bellafebrianyn <bellafebrianynws@gmail.com>
2024-03-02 10:52:40 +0700

    add feature circle

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
+import kotlin.math.PI
+
+class Lingkaran(val radius: Double) {
+
+    fun luas(): Double {
+        return PI * radius * radius
+    }
+
+    fun keliling(): Double {
+        return 2 * PI * radius
+    }
+}
+
 fun main() {
-    println("Hello World!")
+    val lingkaran = Lingkaran(radius = 5.0)
+    println("Luas lingkaran: ${lingkaran.luas()}")
+    println("Keliling lingkaran: ${lingkaran.keliling()}")
 }
